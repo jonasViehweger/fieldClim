16f86293a227c57a50d474c9f0207972bc80f195
langdav <56968651+langdav@users.noreply.github.com>
2020-07-15 18:58:32 +0200

    added turbulence stuff to monthly_climate.R

--- R/monthly_climate.R
+++ R/monthly_climate.R
@@ -8,13 +8,45 @@ monthly_climate <- function(data,
                             v2,
                             p = NULL,
                             elev,
-                            type = "wiese",
+                            surface_type = "wiese",
                             obs_height = 0.3,
-                            hum_rel
+                            hum_rel){
+  t1 <- data[,t1]
+  t2 <- data[,t2]
+  v1 <- data[,v1]
+  v2 <- data[,v2]
+  z1 <- data[,z1]
+  z2 <- data[,z2]
 
-                            ) {
+  #roughness length
+  z0 <- turb_roughness_length(surface_type = surface_type, obs_height = obs_height)
 
+  #displacement height
+  d0 <- turb_displacement(obs_height = obs_height)
 
+  #friction velocity
+  ustar <- turb_ustar(v1 = v1, z1 = z1, z0 = z0)
 
+  #pressure
+  p1 <- pres_p((elev+z1),t1)
+  p2 <- pres_p((elev+z2),t2)
+
+  #air density
+  air_density <- pres_air_density(p1,t1)
+
+  #Gradient-Richardson-Number
+  grad_rich_no <- turb_flux_grad_rich_no(t1,t2,z1,z2,v1,v2,p1,p2)
+
+  #stability
+  stability <- turb_flux_stability(grad_rich_no)
+
+  #Monin-Obhukov-Length
+  mol <- turb_flux_mol(stability,z1,z2,z0,v1,v2,t1,t2,ustar)
+
+  #exchange quotient
+  ex_quotient <- turb_flux_ex_quotient_imp(stability,ustar,mol,z1,air_density)
+
+  #turbulent impulse exchange
+  turb_flux <- turb_flux_imp_exchange(ex_quotient,v1,v2,z1,z2)
 
 }
