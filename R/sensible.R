ec91ce7deddcdce5cc64690d33360e1f2c96d628
jonasViehweger <53001455+jonasViehweger@users.noreply.github.com>
2020-07-15 11:12:06 +0200

    sensibel diese funktion

--- R/sensible.R
+++ R/sensible.R
@@ -7,3 +7,30 @@ QH_TP <- function(temp, rad_bal, soil_flux){
   QH_TP <- ((1-alpt)*sc+lamb)*(-rad_bal-soil_flux)/(sc+lamb);
   return(QH_TP)
 }
+
+
+##################################
+###Fuehlbarer Waermestrom in W/m? #
+##################################
+# p       = Luftdichte (kg/m3), x1
+# mo      = Monin Obukhov Laenge X2
+# cp      =
+# t_gradient   =  Temperaturgradient (dt/dlog(z)), x3;
+# ustern       = Friction velocity , x4
+# ri      x5
+# z = h?he x6
+QH <- function(p,mo,t_gradient,ustar,ri,z) {
+  cp <- 1004.834                  # Spezifische Verdunstungswaerme, T in ?C
+  k <- 0.4
+  s1 <- z/mo
+  if(ri < 0) {Busi <- 0.95*(1-(11.6*s1))^-0.5}      # labil
+  if(ri > 0) {Busi <- 0.95+(7.8*s1) }               # stabile
+  QH <- -1*((p*cp*k*ustar)/Busi)*1*t_gradient;
+  return(QH)
+}
+
+# QH_BO Snesible heat flux form Bo method, x1 rad balance, x2 bodnw?rmestrom , x3 Bow2
+QH_BO <- function(x1,x2,x3){
+  QH_BO <- (-x1-x2) * (x3 / (1+x3));
+  return(QH_BO)
+}
