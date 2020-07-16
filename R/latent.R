7f9a4480b438c1a2e85bb03dd476d68bfed99327
jonasViehweger <53001455+jonasViehweger@users.noreply.github.com>
2020-07-16 14:56:36 +0200

    fixes #6

--- R/latent.R
+++ R/latent.R
@@ -29,10 +29,10 @@ latent_priestley_taylor <- function(t1, rad_bal, soil_flux){
 #' @param datetime POSIXt object (POSIXct, POSIXlt).
 #' @param v1 Wind velocity in m/s.
 #' @param t1 Temperature in degrees C
-#' @param h1 Relative humidity in percent (0-100)
+#' @param hum1 Relative humidity in %.
 #' @param z1 Height of measurement for t1, v1 in m.
 #' @param rad_bal Radiation balance in W/m^2.
-#' @param elevation Elevation above sea level in m.
+#' @param elev Elevation above sea level in m.
 #' @param lat Latitude in decimal degrees.
 #' @param lon Longitude in decimal degrees.
 #'
@@ -41,8 +41,8 @@ latent_priestley_taylor <- function(t1, rad_bal, soil_flux){
 #' @export
 #'
 latent_penman <- function(datetime,
-                       v1, t1, h1, z1, rad_bal,
-                       elevation, lat, lon){
+                       v1, t1, hum1, z1 = 2, rad_bal,
+                       elev, lat, lon){
   if(!inherits(datetime, "POSIXt")){
     stop("datetime has to be of class POSIXt.")
   }
@@ -54,13 +54,13 @@ latent_penman <- function(datetime,
   ut <- lt$hour + lt$min/60 + lt$sec/3600
 
   WeatherStation  <- data.frame(wind=v1,
-                                RH=rel_hum,
+                                RH=hum1,
                                 temp=t1,
                                 radiation=rad_bal,
                                 height=z1,
                                 lat=lat,
                                 long=lon,
-                                elev=elevation)
+                                elev=elev)
 
   lv <- hum_evap_heat(t1)  # Spezifische Verdunstungsw?rme
   QE_PM <- lv*(water::hourlyET(WeatherStation, hours=ut, DOY=doy, long.z=long)/3600)*(-1)
@@ -85,21 +85,21 @@ latent_penman <- function(datetime,
 #' @param z2 Upper height of measurement in m.
 #' @param monin Monin-Obukhov-Length in m.
 #' @param ustar Friction velocity in m/s.
-#' @param ri Gradient-Richardson-Number.
+#' @param grad_rich_no Gradient-Richardson-Number.
 #'
 #' @return Latent heat flux in W/m^2.
 #' @export
 latent_monin <- function(hum1, hum2, t1, t2, p1, p2, z1 = 2, z2 = 10,
-                         monin, ustar, ri) {
+                         monin, ustar, grad_rich_no) {
 
   moist_gradient <- hum_moisture_gradient(hum1, hum2, t1, t2, p1, p2, z1, z2)
   air_density <- pres_air_density(p1, t1)
 
-  lv <- hum_evap_heat(t1)                  # Spezifische Verdunstungswaerme, T in ?C
+  lv <- hum_evap_heat(t1)
   k <- 0.4
   s1 <- z1/monin
-  busi[ri <= 0] <- 0.95*(1-(11.6*s1))^-0.5      # labil
-  busi[ri > 0] <- 0.95+(7.8*s1)                # stabile
+  busi[grad_rich_no <= 0] <- 0.95*(1-(11.6*s1))^-0.5
+  busi[grad_rich_no > 0] <- 0.95+(7.8*s1)
   QL <- -1*((air_density*lv*k*ustar)/busi)*1*moist_gradient
   return(QL)
 }
@@ -113,37 +113,32 @@ latent_monin <- function(hum1, hum2, t1, t2, p1, p2, z1 = 2, z2 = 10,
 #'
 #' @param t1 Temperature at lower height (e.g. height of anemometer) in degrees C.
 #' @param t2 Temperature at upper height in degrees C.
-#' @param h1 Relative humidity at lower height (e.g. height of anemometer) in %.
-#' @param h2 Relative humidity at upper height in %.
+#' @param hum1 Relative humidity at lower height (e.g. height of anemometer) in %.
+#' @param hum2 Relative humidity at upper height in %.
+#' @param p1 Air pressure at lower height in hPa.
+#' @param p2 Air pressure at upper height in hPa.
 #' @param z1 Lower height of measurement (e.g. height of anemometer) in m.
 #' @param z2 Upper height of measurement in m.
 #' @param rad_bal Radiation balance in W/m^2.
 #' @param soil_flux Soil flux in W/m^2.
-#' @param p OPTIONAL. Air pressure in hPa. If not given, elev has to be set.
-#' @param elev OPTIONAL. Elevation above sea level in m. If not given, p has to be set.
 #'
 #' @return Latent heat flux in W/m^2
 #' @export
 #'
-latent_bowen <- function(t1, t2, h1, h2, z1, z2,
-                      rad_bal, soil_flux,
-                      p = NULL, elev = NULL){
-
-  # Calculating pressure
-  if(is.null(p) & is.null(elev)) stop("Either 'p' or 'elev' have to be set.")
-  if(is.null(p)) p <- pres_p(altitude, temp)
+latent_bowen <- function(t1, t2, hum1, hum2, p1, p2, z1 = 2, z2 = 10,
+                         rad_bal, soil_flux){
 
   # Calculating potential temperature delta
-  t1_pot <- temp_pot_temp(t1, p)
-  t2_pot <- temp_pot_temp(t2, p)
+  t1_pot <- temp_pot_temp(t1, p1)
+  t2_pot <- temp_pot_temp(t2, p2)
   dpot <- (t1_pot-t2_pot) / (z1-z2)
 
-  # Calculating absolute humidity
-  af1 <- hum_absolute(hum_vapor_pres(h1, svp(t1)), t1_pot)
-  af2 <- hum_absolute(hum_vapor_pres(h2, svp(t2)), t2_pot)
+  # Calculating absolute humidity delta
+  af1 <- hum_absolute(hum_vapor_pres(hum1, svp(t1)), t1_pot)
+  af2 <- hum_absolute(hum_vapor_pres(hum2, svp(t2)), t2_pot)
   dah <- (af1-af2) / (z1-z2)
 
-  # calculate bowen ratio
+  # Calculate bowen ratio
   bowen_ratio <- bowen_ratio(t1_pot, dpot, dah)
   return((-rad_bal-soil_flux) / (1+bowen_ratio))
 }
