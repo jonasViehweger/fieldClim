ba8967550414abcc846f2de90387bcf8d8c485bb
jonasViehweger <53001455+jonasViehweger@users.noreply.github.com>
2020-07-16 15:49:08 +0200

    fixes #12

--- R/temperature.R
+++ R/temperature.R
@@ -1,17 +1,17 @@
 #' Potential Temperature.
-#' 
+#'
 #' Calculation of the potential air temperature.
 #'
 #' @param p Pressure in hPa.
-#' @param t1 Temperature in °C.
+#' @param t1 Temperature in degrees C.
 #'
-#' @return 
+#' @return Potential temperature in K.
 #' @export
 #'
 #' @examples
 temp_pot_temp <- function(t1, p){
   p0 <- 1013.25    # Standardruck in hPa
   air_const <- 0.286     # spezifische Gaskonstante / spezifische Wärmekapatität; Wert für Luft
-  pot_temp <- t1*(p0/p)**air_const
+  pot_temp <- (t1+273.15)*(p0/p)**air_const
   return(pot_temp)
 }
