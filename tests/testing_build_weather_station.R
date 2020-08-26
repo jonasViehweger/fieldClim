7ac7f5194a3e893c8b6e06091fc1adc7d85c03e0
langdav <56968651+langdav@users.noreply.github.com>
2020-08-26 13:15:58 +0200

    rebuilding example-data

--- tests/testing_build_weather_station.R
+++ tests/testing_build_weather_station.R
@@ -1,25 +1,28 @@
 test_data <- weather_station_example_data
 
-test_data$datetime <- as.POSIXct(test_data$datetime)
-test_data$Albedo <- as.character(test_data$Albedo)
-test_data$Albedo <- as.numeric(test_data$Albedo)
+#test_data$datetime <- as.POSIXct(test_data$datetime)
+#test_data$albedo <- as.character(test_data$albedo)
+#test_data$albedo <- as.numeric(test_data$albedo)
+
+#test_data$heatflux_soil <- as.character(test_data$heatflux_soil)
+#test_data$heatflux_soil <- as.numeric(test_data$heatflux_soil)
 
 test_station <- build_weather_station(z1 = 2,
                                       z2 = 10,
                                       datetime = test_data$datetime,
-                                      t1 = test_data$Ta_2m,
-                                      t2 = test_data$Ta_10m,
-                                      v1 = test_data$Windspeed_2m,
-                                      v2 = test_data$Windspeed_10m,
-                                      hum1 = test_data$Huma_2m,
-                                      hum2 = test_data$Huma_10m,
-                                      albedo = test_data$Albedo,
-                                      t_surface = test_data$Ts,
+                                      t1 = test_data$t1,
+                                      t2 = test_data$t2,
+                                      v1 = test_data$v1,
+                                      v2 = test_data$v2,
+                                      hum1 = test_data$hum1,
+                                      hum2 = test_data$hum2,
+                                      albedo = test_data$albedo,
+                                      t_surface = test_data$t_surface,
                                       texture = "clay",
                                       depth1 = 0.1,
                                       depth2 = 0.3,
-                                      ts1 = test_data$Ts,
-                                      ts2 = test_data$Ts-0.1,
+                                      ts1 = test_data$t_surface,
+                                      ts2 = test_data$t_surface-0.1,
                                       moisture = test_data$water_vol_soil)
 
 
