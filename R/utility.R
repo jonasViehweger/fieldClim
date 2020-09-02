890b134ce8e449b6393018aff301e95109c70a51
jonasViehweger <53001455+jonasViehweger@users.noreply.github.com>
2020-09-02 16:02:15 +0200

    added features as.data.frame

--- R/utility.R
+++ R/utility.R
@@ -25,65 +25,61 @@ check_availability <- function(weather_station, ...){
 #' Create a data.frame from a weather station object, that contains weather station measurements.
 #'
 #' @param weather_station Object of class weather_station.
+#' @param reduced TRUE, to only output the most important columns.
+#' @param units TRUE, to generate longer column labels with units.
 #'
 #' @return data.frame
 #' @export
 #'
-as.data.frame.weather_station <- function(weather_station){
+as.data.frame.weather_station <- function(weather_station, reduced = F, units = F){
 
-  out <- data.frame(datetime = weather_station$measurements$datetime,
-                    t1 = weather_station$measurements$t1,
-                    t2 = weather_station$measurements$t2,
-                    v1 = weather_station$measurements$v1,
-                    v2 = weather_station$measurements$v2,
-                    p1 = weather_station$measurements$p1,
-                    p2 = weather_station$measurements$p2,
-                    hum1 = weather_station$measurements$hum1,
-                    hum2 = weather_station$measurements$hum2,
-                    soil_flux = weather_station$measurements$soil_flux,
-                    sw_in = weather_station$measurements$sw_in,
-                    sw_out = weather_station$measurements$sw_out,
-                    lw_in = weather_station$measurements$lw_in,
-                    lw_out = weather_station$measurements$lw_out,
-                    sw_radiation_balance = weather_station$measurements$sw_bal,
-                    lw_radiation_balance = weather_station$measurements$lw_bal,
-                    total_radiation_balance = weather_station$measurements$rad_bal,
-                    stability_of_atmosphere = weather_station$measurements$stability
-                    #sensible_heat_priestly_taylor = weather_station$measurements$sensible_heat_Priestley_Taylor,
-                    #latent_heat_priestly_taylor = weather_station$measurements$latent_heat_Priestley_Taylor,
-                    #sensible_heat_bowen = weather_station$measurements$sensible_heat_Bowen,
-                    #latent_heat_bowen = weather_station$measurements$latent_heat_Bowen,
-                    #sensible_heat_monin = weather_station$measurements$sensible_heat_Monin,
-                    #latent_heat_monin = weather_station$measurements$latent_heat_Monin,
-                    #latent_heat_penman = weather_station$measurements$latent_heat_Penman
-  )
+  out <- as.data.frame(weather_station$measurements)
 
-  colnames(out) <- c("datetime",
-                     "temperature_lower[°C]",
-                     "temperature_upper[°C]",
-                     "wind_speed_lower[m/s]",
-                     "wind_speed_upper[m/s]",
-                     "pressure_lower[hPa]",
-                     "pressure_upper[hPa]",
-                     "humidity_lower[%]",
-                     "humidity_upper[%]",
-                     "soil_flux[W/m^2]",
-                     "shortwave_radiation_in[W/m^2]",
-                     "shortwave_radiation_out[W/m^2]",
-                     "longwave_radiation_in[W/m^2]",
-                     "longwave_radiation_out[W/m^2]",
-                     "shortwave_radiation_balance[W/m^2]",
-                     "longwave_radiation_balance[W/m^2]",
-                     "total_radiation_balance[W/m^2]",
-                     "atmospheric_stability"
-                     #"sensible_heat[W/m^2]_Priestly-Taylor",
-                     #"latent_heat[W/m^2]_Priestly-Taylor",
-                     #"sensible_heat[W/m^2]_Bowen",
-                     #"latent_heat[W/m^2]_Bowen",
-                     #"sensible_heat[W/m^2]_Monin",
-                     #"latent_heat[W/m^2]_Monin",
-                     #"latent_heat[W/m^2]_Penman"
-                     )
+  # Define important columns
+  important <- c("datetime", "t1", "t2", "v1", "v2", "p1", "p2", "hum1", "hum2", "soil_flux",
+                 "sw_in", "sw_out", "lw_in", "lw_out", "sw_bal", "lw_bal", "rad_bal", "stability",
+                 "sensible_priestley_taylor", "latent_priestley_taylor",
+                 "sensible_bowen", "latent_bowen",
+                 "sensible_monin", "latent_monin",
+                 "latent_penman")
+
+  if(reduced){
+    out <- out[,important[important %in% colnames(out)]]
+  }
+
+  if(long_names){
+    replacement <- c("datetime",
+                      "temperature_lower[°C]",
+                      "temperature_upper[°C]",
+                      "wind_speed_lower[m/s]",
+                      "wind_speed_upper[m/s]",
+                      "pressure_lower[hPa]",
+                      "pressure_upper[hPa]",
+                      "humidity_lower[%]",
+                      "humidity_upper[%]",
+                      "soil_flux[W/m^2]",
+                      "shortwave_radiation_in[W/m^2]",
+                      "shortwave_radiation_out[W/m^2]",
+                      "longwave_radiation_in[W/m^2]",
+                      "longwave_radiation_out[W/m^2]",
+                      "shortwave_radiation_balance[W/m^2]",
+                      "longwave_radiation_balance[W/m^2]",
+                      "total_radiation_balance[W/m^2]",
+                      "atmospheric_stability",
+                      "sensible_heat[W/m^2]_Priestly-Taylor",
+                      "latent_heat[W/m^2]_Priestly-Taylor",
+                      "sensible_heat[W/m^2]_Bowen",
+                      "latent_heat[W/m^2]_Bowen",
+                      "sensible_heat[W/m^2]_Monin",
+                      "latent_heat[W/m^2]_Monin",
+                      "latent_heat[W/m^2]_Penman"
+    )
+
+    for(i in seq_along(important)){
+      names(out)[names(out) == important[i]] <- replacement[i]
+    }
+
+  }
 
   return(out)
 
